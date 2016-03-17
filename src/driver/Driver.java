0255d2207cd0b3c5d98648e6330b36bbf04e0eb3
Vihan Chaudhry <vihanchaudhry@gmail.com>
2016-03-17 16:44:35 -0700

    Optimize imports

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -7,7 +7,6 @@ import server.AutoServerSocket;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.UnknownHostException;
 
 /**
